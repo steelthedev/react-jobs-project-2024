e349612b3aced3c480d39966b6363911b97b4c13
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-10-29 20:41:08 +0100

    committed project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+     <Route index element={<HomePage />} />
+  </Route>
+ 
+)
+)
+
+const App = () => {
+  return <RouterProvider router={router} />
+   
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import Hero from "../components/Hero"
+import HomeCards from "../components/HomeCards"
+import JobListings from "../components/JobListings"
+import ViewAllJobs from "../components/ViewAllJobs"
+
+
+
+const HomePage = () => {
+  return (
+    <>
+   
+    <Hero  />
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs />
+    </>
+  )
+}
+
+export default HomePage
