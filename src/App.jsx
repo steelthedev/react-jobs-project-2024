85e2be491ad8784a6279d9b5bedc50ca4dc99cbc
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-10-31 00:55:43 +0100

    fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 
 
@@ -8,6 +10,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
      <Route index element={<HomePage />} />
+     <Route path='/jobs' element={<JobsPage />} />
+     <Route path='*' element={<NotFoundPage />} />
   </Route>
  
 )
